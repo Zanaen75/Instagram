becad7a4ffad4635909dc2f11b33e9889fe730e5
Zanaen Ullah <zanaenullah75@gmail.com>
2024-06-15 14:37:32 +0500

    FeedPost created

---
+++ src/components/FeedPost/FeedPost.tsx
@@ -0,0 +1,143 @@
+import {StyleSheet, Text, View, Image} from 'react-native';
+import Entypo from 'react-native-vector-icons/Entypo';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feather from 'react-native-vector-icons/Feather';
+import colors from '../../theme/colors';
+import fonts from '../../theme/fonts';
+
+const FeedPost = () => {
+  return (
+    <View style={styles.post}>
+      {/* Header */}
+      <View style={styles.header}>
+        <Image
+          source={{
+            uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
+          }}
+          style={styles.userAvatar}
+        />
+        <Text style={styles.userName}>Zanaen</Text>
+        <Entypo
+          name="dots-three-horizontal"
+          size={16}
+          style={styles.threeDots}
+        />
+      </View>
+      {/* Content */}
+      <Image
+        source={{
+          uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
+        }}
+        style={styles?.image}
+      />
+
+      {/* Footer */}
+      <View style={styles.footer}>
+        <View style={styles.iconContainer}>
+          <AntDesign
+            name="hearto"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Ionicons
+            name="chatbubble-outline"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Feather
+            name="send"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Feather
+            name="bookmark"
+            size={24}
+            style={[{marginLeft: 'auto'}, styles.icon]}
+            color={colors.black}
+          />
+        </View>
+        <Text style={styles.text}>
+          Liked by <Text style={styles.bold}>Hammad</Text> and{' '}
+          <Text style={styles.bold}>69 others</Text>
+        </Text>
+        {/* Post Description */}
+        <Text style={styles.text}>
+          <Text style={styles.bold}>dssadsadasbghfasdj</Text> Lorem ipsum dolor
+          sit amet consectetur, adipisicing elit. Praesentium dolor quaerat quam
+          quia possimus inventore, quos eum quod quis recusandae sunt a qui,
+          temporibus provident nulla molestiae in natus perferendis.
+        </Text>
+
+        {/* Comments */}
+        <Text>View all 16 comments</Text>
+        <View style={styles.comment}>
+          <Text style={styles.commentText}>
+            <Text style={styles.bold}>dssadsadasbghfasdj</Text> Lorem ipsum
+            dolor sit amet consectetur.
+          </Text>
+          <AntDesign name="hearto" style={styles.icon} color={colors.black} />
+        </View>
+        {/* Posted Date */}
+        <Text>19 December, 2021</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  post: {},
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+  },
+  userAvatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  userName: {
+    fontWeight: fonts.weight.bold,
+    color: colors.black,
+  },
+  threeDots: {
+    marginLeft: 'auto',
+  },
+  footer: {
+    padding: 10,
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    marginBottom: 5,
+  },
+  icon: {
+    marginHorizontal: 5,
+  },
+  text: {
+    color: colors.black,
+    lineHeight: 18,
+  },
+  bold: {
+    fontWeight: fonts.weight.bold,
+  },
+  comment: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  commentText: {
+    color: colors.black,
+    flex: 1,
+    lineHeight: 18,
+  },
+});
+
+export default FeedPost;
